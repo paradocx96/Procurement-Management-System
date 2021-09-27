9e057cf5b093c0ee85cc19d8cb480db2a8fc575c
Navinda Lankesh <adilanga@gmail.com>
2021-09-27 13:11:00 +0530

    PMS-API - Order Repository Implementation

---
+++ pms-api/src/main/java/com/csse/pms/dal/repository/OrderRepository.java
@@ -0,0 +1,14 @@
+package com.csse.pms.dal.repository;
+
+import com.csse.pms.dal.model.OrderModel;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface OrderRepository extends MongoRepository<OrderModel, String> {
+
+    List<OrderModel> findByStatus(String status);
+
+    List<OrderModel> findByMainStatus(String mainStatus);
+}
