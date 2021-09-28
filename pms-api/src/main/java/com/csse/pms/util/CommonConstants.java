0dbfed48fbfb7c820c2a0b0f0bced5cf81b3d8be
Navinda Lankesh <adilanga@gmail.com>
2021-09-29 01:49:57 +0530

    PMS-API - Order - Adapter Implementation

--- pms-api/src/main/java/com/csse/pms/util/CommonConstants.java
+++ pms-api/src/main/java/com/csse/pms/util/CommonConstants.java
@@ -40,4 +40,74 @@ public class CommonConstants {
 
     /** Constant for Put Update Status - Mapping Path in Controller */
     public static final String PUT_MAPPING_UPDATE_STATUS = "updateStatus";
+
+    /** Constant for Message - Order Purchase Successfully */
+    public static final String ORDER_PURCHASE_SUCCESSFULLY = "Order Purchase Successfully";
+
+    /** Constant for Message - Order Delete Successfully */
+    public static final String ORDER_DELETE_SUCCESSFULLY = "Order Delete Successfully";
+
+    /** Constant for Message - Order Archive Successfully */
+    public static final String ORDER_ARCHIVE_SUCCESSFULLY = "Order Archive Successfully";
+
+    /** Constant for Message - Order Update Successfully */
+    public static final String ORDER_UPDATE_SUCCESSFULLY = "Order Update Successfully";
+
+    /** Constant for Message - Order Status Update Successfully */
+    public static final String ORDER_STATUS_UPDATE_SUCCESSFULLY = "Order Status Update Successfully";
+
+    /** Constant for Message - Order Purchase Error */
+    public static final String ORDER_PURCHASE_ERROR = "Order Purchase Error";
+
+    /** Constant for Message - Order Delete Error */
+    public static final String ORDER_DELETE_ERROR = "Order Delete Error";
+
+    /** Constant for Message - Order Archive Error */
+    public static final String ORDER_ARCHIVE_ERROR = "Order Archive Error";
+
+    /** Constant for Message - Order Update Error */
+    public static final String ORDER_UPDATE_ERROR = "Order Update Error";
+
+    /** Constant for Message - Order Status Update Error */
+    public static final String ORDER_STATUS_UPDATE_ERROR = "Order Status Update Error";
+
+    /** Constant for Message - Order Doesn't Exist */
+    public static final String ORDER_DOES_NOT_EXIST = "Order Does not Exist";
+
+    /** Constant for Variable id */
+    public static final String ID = "id";
+
+    /** Constant for Variable status */
+    public static final String STATUS = "status";
+
+    /** Constant for Variable referenceNo */
+    public static final String REFERENCE_NO = "referenceNo";
+
+    /** Constant for Variable supplierId */
+    public static final String SUPPLIER_ID= "supplierId";
+
+    /** Constant for Variable itemList */
+    public static final String ITEM_LIST= "itemList";
+
+    /** Constant for Variable siteManagerId */
+    public static final String SITE_MANAGER_ID= "siteManagerId";
+
+    /** Constant for Variable siteId */
+    public static final String SITE_ID= "siteId";
+
+    /** Constant for Variable projectId */
+    public static final String PROJECT_ID= "projectId";
+
+    /** Constant for Variable amount */
+    public static final String AMOUNT= "amount";
+
+    /** Constant for Variable contactDetails */
+    public static final String CONTACT_DETAILS= "contactDetails";
+
+    /** Constant for Variable comment */
+    public static final String COMMENT= "comment";
+
+    /** Constant for Variable dateTime */
+    public static final String DATE_TIME= "dateTime";
+
 }
