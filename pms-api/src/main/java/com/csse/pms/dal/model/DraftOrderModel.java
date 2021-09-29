a90fe83883f05bd854af18147022a33ae1bbdc08
Navinda Lankesh <adilanga@gmail.com>
2021-09-29 11:47:34 +0530

    PMS-API - Draft Order - Model Implementation

---
+++ pms-api/src/main/java/com/csse/pms/dal/model/DraftOrderModel.java
@@ -0,0 +1,134 @@
+package com.csse.pms.dal.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Document
+public class DraftOrderModel {
+
+    @Id
+    private String id;
+    private String supplierId;
+    private List<OrderItemModel> itemList;
+    private String siteManagerId;
+    private String siteId;
+    private String projectId;
+    private double amount;
+    private String contactDetails;
+    private String comment;
+    private LocalDateTime dateTime;
+
+    public DraftOrderModel() {
+    }
+
+    public DraftOrderModel(String id, String supplierId, List<OrderItemModel> itemList, String siteManagerId, String siteId, String projectId, double amount, String contactDetails, String comment, LocalDateTime dateTime) {
+        this.id = id;
+        this.supplierId = supplierId;
+        this.itemList = itemList;
+        this.siteManagerId = siteManagerId;
+        this.siteId = siteId;
+        this.projectId = projectId;
+        this.amount = amount;
+        this.contactDetails = contactDetails;
+        this.comment = comment;
+        this.dateTime = dateTime;
+    }
+
+    @Override
+    public String toString() {
+        return "DraftOrderModel{" +
+                "id='" + id + '\'' +
+                ", supplierId='" + supplierId + '\'' +
+                ", itemList=" + itemList +
+                ", siteManagerId='" + siteManagerId + '\'' +
+                ", siteId='" + siteId + '\'' +
+                ", projectId='" + projectId + '\'' +
+                ", amount=" + amount +
+                ", contactDetails='" + contactDetails + '\'' +
+                ", comment='" + comment + '\'' +
+                ", dateTime=" + dateTime +
+                '}';
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getSupplierId() {
+        return supplierId;
+    }
+
+    public void setSupplierId(String supplierId) {
+        this.supplierId = supplierId;
+    }
+
+    public List<OrderItemModel> getItemList() {
+        return itemList;
+    }
+
+    public void setItemList(List<OrderItemModel> itemList) {
+        this.itemList = itemList;
+    }
+
+    public String getSiteManagerId() {
+        return siteManagerId;
+    }
+
+    public void setSiteManagerId(String siteManagerId) {
+        this.siteManagerId = siteManagerId;
+    }
+
+    public String getSiteId() {
+        return siteId;
+    }
+
+    public void setSiteId(String siteId) {
+        this.siteId = siteId;
+    }
+
+    public String getProjectId() {
+        return projectId;
+    }
+
+    public void setProjectId(String projectId) {
+        this.projectId = projectId;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getContactDetails() {
+        return contactDetails;
+    }
+
+    public void setContactDetails(String contactDetails) {
+        this.contactDetails = contactDetails;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+}
