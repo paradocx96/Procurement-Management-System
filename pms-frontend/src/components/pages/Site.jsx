b2ed733070c9911671ae9c7a263340d65ae6e2da
Ravindu Wataketiya <57728932+RavinduLa@users.noreply.github.com>
2021-10-01 12:17:29 +0530

    Added site page.

---
+++ pms-frontend/src/components/pages/Site.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+class Site extends React.Component{
+    constructor(props) {
+        super(props);
+
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>Site Page</h2>
+
+
+            </div>
+        );
+    }
+
+}
+export default Site;
