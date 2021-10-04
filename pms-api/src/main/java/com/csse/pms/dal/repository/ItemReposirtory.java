bcc09cbaa8b5c9813297d992716fb663af347188
DeclanChirush <hirushgimhan2016@gmail.com>
2021-10-04 11:19:45 +0530

    Create item repository!

---
+++ pms-api/src/main/java/com/csse/pms/dal/repository/ItemReposirtory.java
@@ -0,0 +1,11 @@
+package com.csse.pms.dal.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.csse.pms.dal.model.ItemModel;
+
+@Repository
+public interface ItemReposirtory extends MongoRepository<ItemModel, String>{
+
+}
